f498890321ffa4b59366783a307393d72aa5fe15
unknown <christinesoto21@gmail.com>
2020-02-25 10:59:42 -0600

    Initial Commit

---
+++ Custom List Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom_List_Project
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(2);
+            numbers.Add(4);
+            numbers.Add(6);
+            numbers.Add(8);
+            numbers.Add(10);
+
+        }
+    }
+}
